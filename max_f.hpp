195fcd62ab24a0a7ce8ebc69cd25fbea3f6d1918
mathieutiret <mathieu.y.tiret@gmail.com>
2016-11-29 15:45:25 +0100

    Creating max_f.hpp
    * Creating the struct %like<T>, for easing reading code.
    
    * Creating the function %max_f that looks for the maximum of a function between %min and %max (this function uses a golden section algorithm).

---
+++ max_f.hpp
@@ -0,0 +1,39 @@
+#pragma once 
+
+#include <cmath>
+#include <functional>
+
+template < typename T >
+struct like
+{
+    T value;
+    double likelihood;
+};
+    
+
+template < typename F, typename T >
+auto max_f( F f, T min, T max )
+  -> like<T>
+{
+    // Declaring variables.
+    auto gr = (std::sqrt(5) + 1) / 2;
+    auto tol = 1e-5;
+    auto a = min;
+    auto b = max;
+    
+    auto c = b - (b - a) / gr;
+    auto d = a + (b - a) / gr;
+    
+    while( std::abs(c - d) > tol )
+    {
+      if( f(c) > f(d) )
+        b = d;
+      else
+        a = c;
+      
+      c = b - (b - a) / gr;
+      d = a + (b - a) / gr;
+    }
+        
+    return {(b + a) / 2, f((b + a) / 2)};
+}
