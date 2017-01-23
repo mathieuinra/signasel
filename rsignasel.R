eced4a4fe3737c734325088a91a67bdfced71c76
mathieutiret <mathieu.y.tiret@gmail.com>
2017-01-23 17:24:50 +0100

    Updating rsignasel.R
    - Adding some comments.
    - Changing some function names.
    - Adding the prior as a parameter.
    - Making the sampling function a choice between hypergeometric and binomial samplings.

--- rsignasel.R
+++ rsignasel.R
@@ -1,66 +1,84 @@
 ## Functions
 pij = function( i, px, N, s ) 
 {
-  pxprime = 1. * px * ( s*(px+1.) + 1. )/(2.*s*px + 1.)
+  pxprime = px * ( s*(px+1) + 1 )/( 2*s*px + 1 )
   return( dbinom( i, 2*N, pxprime ) )
 }
 
 
-probability_matrix = function( N, s, ng )
+probability.matrix = function( N, s, ng )
 {
   mat = matrix(NA, nrow = 2*N+1, ncol = 2*N+1)
   
+  ## Assigning probability values.
   for( i in 1:nrow(mat) )
     for( j in 1:ncol(mat) )
       mat[i,j] = pij( i-1, 1.*(j-1)/2/N, N, s )
-    
-    res = mat
-    
+  
+  ## Matrix multiplication ~ res = mat^(ng-1)
+  res = mat
+  if( ng >= 2 )
+  {
     for( g in 2:ng )
       res = res %*% mat
+  }
     
-    return(res)
+  return(res)
 }
 
 
-r.__likelihood = function( i1, S1, i2, S2, N, ng, s )
+impl.likelihood = function( p0, i1, S1, i2, S2, N, ng, s, ps1.replacement = T, ps2.replacement = T )
 {
-  mat = probability_matrix(N, s, ng)
+  mat = probability.matrix(N, s, ng)
   
-  prob_i1 = 1./(2*N+1-i1)
-  
-  # Declaring miscellaneous variables.
   L = 0.
   
-  for( i in i1:(2*N) ) 
+  for( k1 in 0:(2*N) ) 
   {
     # probability of sampling at t1
-    ps1 = dhyper( i1, i, 2*N-i, 2*S1 )
-    
+    if(ps1.replacement)
+    {
+      ps1 = p0[k1] * dbinom( i1, 2*S1, k1/2/N ) # with replacement
+    } else
+      ps1 = p0[k1] * dhyper( i1, k1, 2*N-k1, 2*S1 ) # without replacement
+      
+      
     # probability of sampling at t2
     ps2 = 0.
-    for( k in i2:(2*N) )
-      ps2 = ps2 + mat[k+1,i+1] * dhyper( i2, k, 2*N-k, 2*S2 )
+    for( k2 in 0:(2*N) )
+    {
+      if(ps2.replacement)
+      {
+        tmp = dbinom( i2, 2*S2, k2/2/N ) 
+      } else
+        tmp = dhyper( i2, k2, 2*N-k2, 2*S2 )
+        
+      ps2 = ps2 + mat[k2+1,k1+1] * tmp
+     }
     
     L = L + ps1 * ps2
-  }
+  }   
   
-  return(L * prob_i1)
+  return(L)
 }
 
 ## Main function
-r.likelihood = function( data, N, s )
+likelihood = function( data, N, s, p0 = NULL )
 {
+  if( is.null(p0) )
+    p0 = rep(1/(2*N+1),2*N+1)
+  
   p2 = 1.
   
-  for( k in 1:(dim(data)[1]-1) )
-    p2 = p2 * r.__likelihood( data[k,2],   # i1
-                              data[k,3],   # S1
-                              data[k+1,2], # i2
-                              data[k+1,3], # S2
-                              N, 
-                              data[k+1,1]-data[k,1], # t2-t1
-                              s )
+  for( k in 1:(nrow(data)-1) )
+    p2 = p2 * impl.likelihood( p0,  
+                               data[k,2],   # i1
+                               data[k,3],   # S1
+                               data[k+1,2], # i2
+                               data[k+1,3], # S2
+                               N, 
+                               data[k+1,1]-data[k,1], # t2-t1
+                               s )
   
   return(p2)
 }
@@ -87,7 +105,7 @@ i2 = rhyper(1, i.tmp, 2*Ne-i.tmp, 2*S2)
 
 data = matrix(c(t1,t2,i1,i2,S1,S2),ncol=3)
 
-like = sapply( 10:20, function(x) r.likelihood(data,x,0) )
+like = sapply( 10:20, function(x) likelihood(data,x,0) )
 plot(like)
 
 
