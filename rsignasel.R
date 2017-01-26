e30c32350c224f0986d934fd2c9988671c7655b4
mathieutiret <mathieu.y.tiret@gmail.com>
2017-01-26 17:54:06 +0100

    Updating rsignasel.R
    - Making the sampling function choice another distinct function, %impl.sampling.fn, extensible in the future.
    - Updating the other functions depending on %impl.sampling.fn.

--- rsignasel.R
+++ rsignasel.R
@@ -26,35 +26,48 @@ probability.matrix = function( N, s, ng )
   return(res)
 }
 
-
-impl.likelihood = function( p0, i1, S1, i2, S2, N, ng, s, ps1.replacement = T, ps2.replacement = T )
+     
+impl.sampling.fn = function( fn.opt )
 {
+  if( is.character(fn.opt) )
+  {
+    if( fn.opt == "dbinom" )
+      fn = function(N,i,S,k) dbinom( i, 2*S, k/2/N )
+    else if( fn.opt == "dhyper" )
+      fn = function(N,i,S,k) dhyper( i, k, 2*N-k, 2*S )
+    else
+      stop("unknown sampling distribution.")
+  } else if( is.function(fn.opt) )
+  {
+    fn = fn.opt
+  } else
+    stop( "sampling option is not correct." )
+  
+  return( fn )
+}
+      
+
+impl.likelihood = function( p0, i1, S1, i2, S2, N, ng, s, 
+                            ps1.fn = "dbinom", ps2.fn = "dbinom" )
+{
+  ## Computing the probability matrix.
   mat = probability.matrix(N, s, ng)
   
-  L = 0.
+  ## Sampling function
+  fn1 = impl.sampling.fn( ps1.fn )
+  fn2 = impl.sampling.fn( ps2.fn )
   
+  ## Computing the likelihood
+  L = 0
   for( k1 in 0:(2*N) ) 
   {
     # probability of sampling at t1
-    if(ps1.replacement)
-    {
-      ps1 = p0[k1+1] * dbinom( i1, 2*S1, k1/2/N ) # with replacement
-    } else
-      ps1 = p0[k1+1] * dhyper( i1, k1, 2*N-k1, 2*S1 ) # without replacement
-      
-      
+    ps1 = p0[k1+1] * fn1(N,i1,S1,k1)
+    
     # probability of sampling at t2
-    ps2 = 0.
+    ps2 = 0
     for( k2 in 0:(2*N) )
-    {
-      if(ps2.replacement)
-      {
-        tmp = dbinom( i2, 2*S2, k2/2/N ) 
-      } else
-        tmp = dhyper( i2, k2, 2*N-k2, 2*S2 )
-        
-      ps2 = ps2 + mat[k2+1,k1+1] * tmp
-     }
+      ps2 = ps2 + mat[k2+1,k1+1] * fn2(N,i2,S2,k2)
     
     L = L + ps1 * ps2
   }   
@@ -63,12 +76,13 @@ impl.likelihood = function( p0, i1, S1, i2, S2, N, ng, s, ps1.replacement = T, p
 }
 
 ## Main function
-likelihood = function( data, N, s, p0 = NULL )
+likelihood = function( data, N, s, 
+                       p0 = NULL, ps1.fn = "dbinom", ps2.fn = "dbinom" )
 {
   if( is.null(p0) )
     p0 = rep(1/(2*N+1),2*N+1)
   
-  p2 = 1.
+  p2 = 1
   
   for( k in 1:(nrow(data)-1) )
     p2 = p2 * impl.likelihood( p0,  
@@ -78,7 +92,8 @@ likelihood = function( data, N, s, p0 = NULL )
                                data[k+1,3], # S2
                                N, 
                                data[k+1,1]-data[k,1], # t2-t1
-                               s )
+                               s, 
+                               ps1.fn, ps2.fn )
   
   return(p2)
 }
