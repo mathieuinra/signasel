1811c7966be0c854e062565ef1c45e56a8c10bec
mathieutiret <mathieu.y.tiret@gmail.com>
2017-02-10 16:19:32 +0100

    Adding install.sh
    Adding a script to install the rcpp version of this package.

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Error control.
+set -o nounset
+set -o errexit
+
+
+# Detecting libgsl
+gsl=$( whereis libgsl.a | cut -f 2 -d ' ' )
+if [ -z $gsl ]
+then 
+    exit 1
+fi
+
+
+# Creating the package
+R -e "library(Rcpp);Rcpp.package.skeleton(\"signasel\", example_code = F)" &> log
+echo -e "PKG_CXXFLAGS=-I/usr/include/ --std=c++1z -static\nPKG_LIBS=$gsl" > Makevars
+cp max_f.hpp signasel.cpp Makevars signasel/src/
+
+# Compiling 
+R -e "library(Rcpp);compileAttributes(\"signasel\")"
+R -e "library(devtools);install(\"signasel\")"
+
+
+
